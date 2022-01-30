078e84d8d0ff611fdb8ef1c68844c7c75e80a34e
rilee <97436315+degurrrrrr@users.noreply.github.com>
2022-01-30 18:43:12 +0900

    1/29까지 진행상황
    커밋은 어려워

--- src/Add.js
+++ src/Add.js
@@ -1,30 +1,55 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 import { useHistory } from "react-router-dom"
 import styled from "styled-components"
-// import App from "./App"
 
 const Add = () => {
   const history = useHistory();
+  
+  const [word, setWord] = useState('');
+  const [announce, setAnnounce] = useState('');
+  const [mean, setMean] = useState('');
+  const [ex, setEx] = useState('');
+
+  const wordInput = useRef(null); 
+  const announceInput = useRef(null); 
+  const meanInput = useRef(null); 
+  const exInput = useRef(null);
+
+  console.log(wordInput.current)
+  console.log(announceInput.current)
+  console.log(meanInput.current)
+  console.log(exInput.current)
+  //null 뜸
+
+
   return (
     // div1 : return 대응 div. 전체 아우르는 div.
-    <div style={{ color: "dimgray", textAlign: "center", height: "100vh" }}>
-      <h1 style={{ display: "inline-flex" }}>📚우리말 단어장📚</h1>
+    <div style={{ color: "dimgray", textAlign: "center", height: "800px"}}>
+      <h1 
+      style={{ cursor: "pointer", display: "grid", margin: "15px auto", width: "100%" }}
+      onClick={() => history.push('/')}
+      >📚우리말 단어장📚</h1>
       <hr />
-      <h3 >단어 추가하기</h3>
+      <h3 style={{ margin: "30px" }} >단어 추가하기</h3>
       {/* div2 : div3 가로 정해주는 용도?!*/}
       <Container>
         {/* div3 */}
         <Contents>
           {/* !!!!게시글 작성에 필요한 input 3개를 ref로 관리하기!!! */}
+          {/* ref는 리액트 컴포넌트에 접근해 원하는 값을 가져올 수 있게 하는?? */}
           {/* !!!!작성 게시글 리덕스 내 게시글 목록에 추가!!!! */}
           {/* !!!!게시글 목록 파이어스토어에 저장하기!!!! */}
-          <h3>단어: <input type="text" style={{ backgroundColor: "ghostwhite", border: "none" }} /></h3>
-          <h3>의미: <input type="text" style={{ backgroundColor: "ghostwhite", border: "none" }} /> </h3>
-          <h3>예문: <input type="text" style={{ backgroundColor: "ghostwhite", border: "none" }} /></h3>
+          <h3>단어 <InputBox ref={wordInput} value={word}/></h3>
+          <h3>발음 <InputBox ref={announceInput} value={announce}/></h3>         
+          <h3>의미 <InputBox ref={meanInput} value={mean}/> </h3>
+          <h3>예문 <InputBox ref={exInput} value={ex}/></h3>
+          {/* map으로 동그라미 그렸듯이 단어, 의미, 예문을 각자 받아와야하나?
+          그래서 저장 후에도 main에 뿌려주고? */}
         </Contents>
         {/* div4 */}
         <AddButton style={{ textAlign: "center" }} onClick={() => {
           history.push('/');
+          window.location.reload();
         }}>
           추가하기 🥰
         </AddButton>
@@ -37,29 +62,46 @@ const Container = styled.div`
 border: solid #eee 2px;
 border-radius: 10px;
 width: 60vw;
-height: 75vh;
+height: 550px;
 text-align: left;
 margin: auto;
-padding-top: 20px;
+padding-top: 0px;
 `;
 // 아니면 Container.h3로 지정 변경하는 방법이 나으려나??
 const Contents = styled.div`
-background-color: ghostwhite;
-margin: 50px 60px;
+margin: 20px 60px;
 padding: 10px;
-text-align: justify;
-// 줄 간격 넓히는 방법?
+text-align: center;
+justify-content: center;
+`;
+
+const InputBox = styled.input`
+color: dimgray;
+border : solid 2px #eee;
+border-radius: 15px;
+/* border-top: none;
+border-left: none;
+border-right: none;
+border-bottom: solid 2px #eee; */
+font-size: 20px;
+width: 90%;
+height: 30px;
+margin-top: 8px;
+margin-bottom: 10px;
+display: inline-block;
+text-align: center;
+//input 안의 text 정렬
 `;
 
 const AddButton = styled.div`
-background-color: dimgray;
+background-color: #ffcc00;
 color: #fff;
 font-weight: bold;
 font-size: 18px;
-width: 250px;
-margin: 20px auto;
+width: 200px;
+margin: 10px auto 20px;
 padding: 10px;
-border-radius: 15px;
+border-radius: 20px;
 cursor: pointer;
 `;
 
--- src/Main.js
+++ src/Main.js
@@ -1,65 +1,118 @@
-import React from "react"
-import { Link, useState, useHistory } from "react-router-dom"
-
+import React, { useState, useEffect } from "react"
+import { useHistory } from "react-router-dom"
 import styled from "styled-components"
 
-import { BsCheckCircle } from "react-icons/bs"
+import { BsCheckCircle, BsPlusLg } from "react-icons/bs"
 import { AiOutlineDelete } from "react-icons/ai"
 import { IoIosAddCircle } from "react-icons/io"
 
-// import App from "./App"
 //여유되면 폰트 바꾸기!!
 
-// 버튼 클릭할 때, 색깔 반전? 및 아이콘 변경 (BsCheckCircleFill)
-// function Checked {
-//   const [check, setCheck] = useState();
-//   return (
-//     e
-//   )
-// }
 const Main = () => {
   const history = useHistory();
-  // const del_alert = alert{"비밀번호를 입력하세요 <input />}
-  return (
-    <div style={{ color: "dimgray", textAlign: "center", height: "100vh" }}>
-      <h1 style={{ display: "inline-flex" }}>📚우리말 단어장📚</h1>
-      <hr />
-      <h3 >단어 목록</h3>
-      {/* 카드 뷰로 만들기??? hover에 모달 효과 */}
-      <div style={{ display: "flex" }}>
-        <WordCard>
-          <h3>단어</h3>
-          <p>[단어(발음)]</p>
-          <p style={{ color: "blue" }}>단어 예문</p>
-          <div style={{ float: "right", cursor: "pointer" }}>
-            <BsCheckCircle /> <AiOutlineDelete style={{ marginLeft: "5px" }} onClick={alert}/>
-            {/* 비밀번호 입력하라는 input+ alert! */}
-          </div>
-        </WordCard>
+  const [color, setColor] = React.useState('white');
+  const [icolor, setIcolor] = React.useState('dimgray');
+
+  function onClick() {
+    color === '#ddd' ? setColor('white') : setColor('#ddd');
+    console.log("checkBtnClick");
+    //function으로 해도 되긴 된다.
+    icolor === 'white' ? setIcolor('dimgray') : setIcolor('white');
+    //아이콘 변경으로 할 수 있는 방법은? 좋아요 토글버튼?
+    console.log("checked");
+  }
+
+    function deleteBtn() {
+      return (
+        prompt("비밀번호를 입력하세요"),
+        alert("비밀번호를 확인하세요"),
+        history.push('/'),
+        
+        console.log("reload")
+      )
+    }
+
+    return (
+      <div style={{ color: "dimgray", textAlign: "center", height: "100vh"}}>
+        <h1 
+        style={{ cursor: "pointer", display: "inline-flex",  margin: "15px auto" }} 
+        onClick={() => window.location.reload()}
+        >📚우리말 단어장📚</h1>
+        <hr />
+        <h3 >단어 목록</h3>
+
+        <div style={{ width: "100vw", display: "flex" }}>
+          <WordCard color={color} >
+            <h2 >단어{' '}<span style={{ fontSize: "medium", fontWeight: "lighter" }}>[단어(발음)]</span></h2>
+            <p>의미</p>
+            <p style={{ color: "blue" }}>단어 예문</p>
+            <div style={{ float: "right", cursor: "pointer" }}>
+              <BsCheckCircle onClick={onClick} size={20} color={icolor} />
+              {/* color는 ㄹㅇ 색상. {위에서 선언한 변수 내지 받아오는 값!!} */}
+              <AiOutlineDelete size={20} style={{ marginLeft: "5px" }} onClick={deleteBtn} />
+            </div>
+          </WordCard>
+        </div>
+
+        {/* <AddButton><BsPlusLg color={'#000'} style={{ }}/></AddButton> */}
+        
+        <CircleButton onClick={() => {
+          history.push('/add')
+        }} >
+          <IoIosAddCircle size={50} />
+        </CircleButton>
+        {/* 나는 로테이션 시키면 동그라미 자체가 오른쪽으로 이동하는 느낌인데, 샘플은 +만 도는 느낌.
+        나도 동그라미를 주고 안에 +만 넣어서 로테이션 시켜볼까 */}
+
       </div>
-      {/* 단어 추가 버튼 */}
-      <div>
-        <IoIosAddCircle
-          size={50}
-          style={{ position: "absolute", right: "0px", bottom: "0px", cursor: "pointer", margin: "0px 10px 10px 0px" }}
-          onClick={() => {
-            history.push('/add');
-          }} />
-      </div>
 
+    );
+  };
 
-    </div>
+const CircleButton = styled.div`
+position: absolute;
+right: 0px; 
+bottom: 0px;
+cursor: pointer;
+margin: 0px 10px 10px 0px;
+&:hover {
+  transition: 0.2s all ease;
+  transform: rotate(90deg);
+  /* animation: backwards; */
+  /* transform-origin: 50% 45% */
+}
+`;
 
-  );
-};
+// const AddButton= styled.div`
+// width: 50px;
+// height: 50px;
+// border-radius: 25px;
+// position: absolute;
+// right: 0px; 
+// bottom: 0px;
+// margin: 0px 10px 10px 0px;
+// background-color: dimgray;
+// cursor: pointer;
+// `;
+
+export default Main;
 
 const WordCard = styled.div`
-width: 25vw;
+width: 250px;
 text-align: left;
 margin : 10px 20px;
 padding: 15px 15px;
 border: solid #eee 2px;
 border-radius: 10px;
+transition: 0.125s all ease;
+&:hover {
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 5px 15px 0px;
+  -webkit-transform: scale(1.05);
+     -moz-transform: scale(1.05);
+      -ms-transform: scale(1.05);
+       -o-transform: scale(1.05);
+          transform: scale(1.05);  
+}
+background-color: ${props => props.color};
 `;
-
-export default Main;
+// 아예 바깥으로 빠져나와야 리렌더링 안되고 색깔 변경 일어난다
